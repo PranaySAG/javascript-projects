23b374c87c2fc7652d61bbfb74b39e084a7088b7
PranaySAG <pranayshinde929@gmail.com>
2025-01-09 22:53:41 +0530

    made some changes in expanding card project

--- expanding card/script.js
+++ expanding card/script.js
@@ -1,14 +1,12 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll(".panel");
 
 panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+    panel.addEventListener("mouseover", () => {
+        panel.classList.add("active");
+    });
 
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
-}
+    panel.addEventListener("mouseout", () => {
+        panel.classList.remove("active");
+    });
+
+})
